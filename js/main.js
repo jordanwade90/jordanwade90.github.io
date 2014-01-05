aeabfe73502597d6a6aa5264cec3465891798070
Jordan Wade <jordan@jwade.net>
2014-01-04 20:24:53 -0600

    Dynamically add navbar collapse button

---
+++ js/main.js
@@ -0,0 +1,4 @@
+if ($("#nav-tail").length > 0) {
+	$("#nav .navbar-header").prepend('<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#nav-tail"><span class="sr-only">Toggle navigation.</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>')
+	$("#nav-tail").addClass("collapse navbar-collapse")
+}
